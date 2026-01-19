22b8665d0646c00b11aedeca4ec94e7d91671a7e
sakur <sakur.cse9.bu@gmail.com>
2026-01-19 19:08:23 +0600

    new commit

---
+++ test.py
@@ -0,0 +1,14 @@
+w = 0.0
+b = 0.0
+x = 1
+y = 7
+lr = 0.1
+
+for step in range(10):
+    y_pred = w * x + b
+    error = y_pred - y
+
+    w = w - lr * error * x
+    b = b - lr * error
+
+    print(step, w, b)
